c8e1abc45483f16a9bff07121cae91cbd9c1bb4e
shadow7919 <malekishayan79@gmail.com>
2020-04-01 10:08:57 +0430

    init rooms

--- Part.java
+++ Part.java
@@ -1,9 +1,14 @@
 package ir.ac.kntu;
 
-public class Part {
-    private int roomsNumber;
-    private PartKind partKind;
+import java.util.*;
 
+public class Part {
+    Scanner scanner = new Scanner(System.in);
+    private int normalRoomsNumber;
+    private int emergencyRoomsNumber;
+    private PartKind partKind;
+    Hospital hospital = new Hospital();
+    Room room = new Room();
     public void setPartKind(PartKind partKind) {
         this.partKind = partKind;
     }
@@ -11,7 +16,30 @@ public class Part {
     public PartKind getPartKind() {
         return partKind;
     }
+    public void makeRooms(){
+        System.out.println("Enter how many room do you need for " + PartKind.NORMAL + "part ");
+        normalRoomsNumber = scanner.nextInt();
+        System.out.println("And also" + PartKind.EMERGENCY + "part rooms?");
+        emergencyRoomsNumber = scanner.nextInt();
+        System.out.println("enter the default number of beds for all rooms");
+        room.setBedsNumber(scanner.nextInt());
+        Map <Integer,Integer> temp = new HashMap();
+        hospital.getNormalRooms().add(temp);
+        for (int i = 1; i <= normalRoomsNumber; i++) {
+            temp.put(i,room.getBedsNumber());
+            hospital.getNormalRooms().add(temp);
+        }
+        for (int i = 1; i <= normalRoomsNumber; i++) {
+            temp.put(i,room.getBedsNumber());
+            hospital.getEmergencyRooms().add(temp);
+        }
+        System.out.println("and the price for the parts");
+        System.out.print(partKind.NORMAL+ " :");
+        room.setNormalPrice(scanner.nextInt());
+        System.out.print(partKind.EMERGENCY+ " :");
+        room.setEmergencyPrice(scanner.nextInt());
+    }
 }
 enum PartKind{
-    emergency,main;
+    EMERGENCY,NORMAL;
 }
--- Room.java
+++ Room.java
@@ -1,10 +1,38 @@
 package ir. ac.kntu;
 
-import java.util.Date;
+import java.util.*;
 
 public class Room {
     private int roomNumber;
-    private int bedsNumber;
+    private int bedsNumber ;
     private boolean isAvailable;
-    private int price;
+    private int normalPrice;
+    private int emergencyPrice;
+
+    public void addRoom(PartKind partKind,Hospital hospital){
+        System.out.println("Enter the room number and beds Number");
+        System.out.println("What's the main price of room");
+        ArrayList<Map> rooms= new ArrayList<>();
+        Map <Integer,Integer> tempRoom = new HashMap();
+        if(partKind == PartKind.NORMAL){
+            hospital.getNormalRooms().add(tempRoom);
+        }else{
+            hospital.getEmergencyRooms().add(tempRoom);
+        }
+    }
+
+    public int getBedsNumber() {
+        return roomNumber;
+    }
+    public void setBedsNumber(int bedsNumber){
+        this.bedsNumber = bedsNumber;
+    }
+
+    public void setNormalPrice(int normalPrice) {
+        this.normalPrice = normalPrice;
+    }
+
+    public void setEmergencyPrice(int emergencyPrice) {
+        this.emergencyPrice = emergencyPrice;
+    }
 }
