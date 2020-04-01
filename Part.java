30869bc4aa5271aaa0f5fe90f75eb4d796622628
shadow7919 <malekishayan79@gmail.com>
2020-04-01 18:26:55 +0430

    added method make room for picking it

--- Part.java
+++ Part.java
@@ -7,8 +7,8 @@ public class Part {
     private int normalRoomsNumber;
     private int emergencyRoomsNumber;
     private PartKind partKind;
-    Hospital hospital = new Hospital();
     Room room = new Room();
+
     public void setPartKind(PartKind partKind) {
         this.partKind = partKind;
     }
@@ -16,30 +16,44 @@ public class Part {
     public PartKind getPartKind() {
         return partKind;
     }
-    public void makeRooms(){
-        System.out.println("Enter how many room do you need for " + PartKind.NORMAL + "part ");
+
+    public void makeRooms(Hospital hospital) {
+        System.out.println("Enter how many room do you need for " + PartKind.NORMAL + " Part ");
         normalRoomsNumber = scanner.nextInt();
-        System.out.println("And also" + PartKind.EMERGENCY + "part rooms?");
+        System.out.println("And also " + PartKind.EMERGENCY + " Part rooms?");
         emergencyRoomsNumber = scanner.nextInt();
         System.out.println("enter the default number of beds for all rooms");
-        room.setBedsNumber(scanner.nextInt());
-        Map <Integer,Integer> temp = new HashMap();
-        hospital.getNormalRooms().add(temp);
-        for (int i = 1; i <= normalRoomsNumber; i++) {
-            temp.put(i,room.getBedsNumber());
-            hospital.getNormalRooms().add(temp);
+        int defaultBedNumber = scanner.nextInt();
+        while (!check(defaultBedNumber)) {
+            if (check(defaultBedNumber)) {
+                room.setBedsNumber(defaultBedNumber);
+            } else {
+                System.out.println("The Max bed each room can have is 6");
+            }
+            defaultBedNumber = scanner.nextInt();
         }
         for (int i = 1; i <= normalRoomsNumber; i++) {
-            temp.put(i,room.getBedsNumber());
-            hospital.getEmergencyRooms().add(temp);
+            hospital.getNormalRooms().put(i,defaultBedNumber);
+        }
+        for (int i = 1; i <= emergencyRoomsNumber; i++) {
+            hospital.getEmergencyRooms().put(i,defaultBedNumber);
         }
         System.out.println("and the price for the parts");
-        System.out.print(partKind.NORMAL+ " :");
+        System.out.print(PartKind.NORMAL + " :");
         room.setNormalPrice(scanner.nextInt());
-        System.out.print(partKind.EMERGENCY+ " :");
+        System.out.print(PartKind.EMERGENCY + " :");
         room.setEmergencyPrice(scanner.nextInt());
     }
+    public void changeRoomsBedNumbers(){
+    }
+    private boolean check(int defaultBedNumber) {
+        if (defaultBedNumber > 0 && defaultBedNumber <= 6) {
+            return true;
+        }
+        return false;
+    }
 }
-enum PartKind{
-    EMERGENCY,NORMAL;
+
+enum PartKind {
+    EMERGENCY, NORMAL;
 }
