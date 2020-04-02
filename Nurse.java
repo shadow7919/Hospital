825d8805f008e7544b7faca131310af4b8a27d1d
shadow7919 <malekishayan79@gmail.com>
2020-04-02 14:50:19 +0430

    add and show the Nurses and their doctor

--- Nurse.java
+++ Nurse.java
@@ -1,10 +1,88 @@
 package ir.ac.kntu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Nurse {
+    Scanner scanner = new Scanner(System.in);
     private String name;
     private int id;
-    private int shiftNumber;
-    private Doctor firstDoctor;
-    private Doctor secondDoctor;
-    private boolean partSource;
+    private ArrayList<Doctor> doctors;
+    private boolean isPartSource;
+
+    public Nurse(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    public Nurse(){}
+
+    public void addNurses(Hospital hospital) {
+        System.out.println("-------- Add Nurse --------");
+        System.out.println("Enter id for new Nurse");
+        int inputId = scanner.nextInt();
+        while (sameId(hospital, inputId) != null) {
+            System.out.println("we have same id saved");
+            inputId = scanner.nextInt();
+        }
+        id = inputId;
+        System.out.print("Enter Name : ");
+        scanner.nextLine();
+        name = scanner.nextLine();
+        System.out.println(name+ " serve in part source ?");
+        System.out.print(YesOrNo.YES + " OR "+ YesOrNo.NO + "  ");
+        String input = scanner.next();
+        YesOrNo yesOrNo ;
+        while (true) {
+            try {
+                yesOrNo = YesOrNo.valueOf(input);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Wrong input ");
+            }
+        }
+        isPartSource = yesOrNo.isInside();
+    }
+    public void showNurses(Hospital hospital){
+        Nurse nurse ;
+        System.out.println("-------- SHOW NURSE --------");
+        System.out.print("Enter the id ");
+        int inputId = scanner.nextInt();
+        while (sameId(hospital,inputId)==null || inputId !=0 ){
+            System.out.println("Either enter the write id or 0");
+            inputId = scanner.nextInt();
+        }
+        nurse = sameId(hospital,inputId);
+        System.out.println("ID : "+nurse.id +" Name : "+nurse.name);
+        if(nurse.isPartSource){
+            System.out.println(name+" work in part source ");
+        }else{
+            if(nurse.doctors.get(0)!= null){
+                System.out.println("The doctor of "+ nurse.name);
+                System.out.println(nurse.doctors.get(0).getId());
+            }
+            if(nurse.doctors.get(1)!= null){
+                System.out.println("The doctor of "+ nurse.name);
+                System.out.println(nurse.doctors.get(0).getId());
+            }
+        }
+    }
+    public Nurse sameId(Hospital hospital, int inputId) {
+        for (Nurse nurse : hospital.getAllNurses()) {
+            if (inputId == nurse.id) {
+                return nurse;
+            }
+        }
+        return null;
+    }
+    private enum YesOrNo{
+        YES(true),NO(false);
+        public boolean inside;
+        YesOrNo(boolean inside){
+            this.inside = inside;
+        }
+
+        public boolean isInside() {
+            return inside;
+        }
+    }
 }
