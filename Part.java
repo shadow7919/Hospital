983b893242d0b7933caab35e6b305970801f7cf7
shadow7919 <malekishayan79@gmail.com>
2020-04-03 10:22:51 +0430

    changes in make room

--- Part.java
+++ Part.java
@@ -1,20 +1,23 @@
 package ir.ac.kntu;
 
-import java.util.*;
+import java.util.Scanner;
+
+enum PartKind {
+    EMERGENCY, NORMAL;
+}
 
 public class Part {
     Scanner scanner = new Scanner(System.in);
     private int normalRoomsNumber;
     private int emergencyRoomsNumber;
     private PartKind partKind;
-    Room room = new Room();
-
-    public void setPartKind(PartKind partKind) {
-        this.partKind = partKind;
-    }
 
     public PartKind getPartKind() {
         return partKind;
+    }
+
+    public void setPartKind(PartKind partKind) {
+        this.partKind = partKind;
     }
 
     public void makeRooms(Hospital hospital) {
@@ -25,35 +28,28 @@ public class Part {
         System.out.println("enter the default number of beds for all rooms");
         int defaultBedNumber = scanner.nextInt();
         while (!check(defaultBedNumber)) {
-            if (check(defaultBedNumber)) {
-                room.setBedsNumber(defaultBedNumber);
-            } else {
-                System.out.println("The Max bed each room can have is 6");
-            }
+            System.out.println("The Max bed each room can have is 6");
             defaultBedNumber = scanner.nextInt();
-        }
-        for (int i = 1; i <= normalRoomsNumber; i++) {
-            hospital.getNormalRooms().put(i,defaultBedNumber);
-        }
-        for (int i = 1; i <= emergencyRoomsNumber; i++) {
-            hospital.getEmergencyRooms().put(i,defaultBedNumber);
         }
         System.out.println("and the price for the parts");
         System.out.print(PartKind.NORMAL + " :");
-        room.setNormalPrice(scanner.nextInt());
+        int normalRoomPrice = scanner.nextInt();
         System.out.print(PartKind.EMERGENCY + " :");
-        room.setEmergencyPrice(scanner.nextInt());
+        int emergencyRoomPrice = scanner.nextInt();
+        for (int i = 1; i <= normalRoomsNumber; i++) {
+            Room room = new Room(i, defaultBedNumber, normalRoomPrice);
+            hospital.getNormalRooms().add(room);
+        }
+        for (int i = 1; i <= emergencyRoomsNumber; i++) {
+            Room room = new Room(i, defaultBedNumber, emergencyRoomPrice);
+            hospital.getEmergencyRooms().add(room);
+        }
     }
-    public void changeRoomsBedNumbers(){
-    }
-    private boolean check(int defaultBedNumber) {
+
+    public boolean check(int defaultBedNumber) {
         if (defaultBedNumber > 0 && defaultBedNumber <= 6) {
             return true;
         }
         return false;
     }
-}
-
-enum PartKind {
-    EMERGENCY, NORMAL;
 }
