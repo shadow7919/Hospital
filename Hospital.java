d3a20539e9dfc06675b4c3ea17da664d0549ad89
shadow7919 <malekishayan79@gmail.com>
2020-04-04 14:59:06 +0430

    delet Hospital address

--- Hospital.java
+++ Hospital.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Hospital {
     private String name;
-    private Address address;
     private ArrayList<Doctor> doctors = new ArrayList<>();
     private ArrayList<Patient> patients = new ArrayList<>();
     private ArrayList<Room> normalRooms = new ArrayList<>();
@@ -36,15 +35,7 @@ public class Hospital {
     public void setName(String name) {
         this.name = name;
     }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String toString() {
-        return name + " " + address;
-    }
-
+    
     public ArrayList<Patient> getPatients() {
         return patients;
     }
@@ -59,29 +50,5 @@ public class Hospital {
 
     public void hospitalMenu() {
         System.out.println("---------- Hospital ----------");
-    }
-}
-
-class Address {
-    private String city;
-    private String street;
-    private String other;
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-
-    public void setOther(String other) {
-        this.other = other;
-    }
-
-
-    public String toString() {
-        return city + " " + street + " " + other;
     }
 }
